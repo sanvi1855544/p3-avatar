4d37543f77d3d44702137128b96b67c455bc8015
Gennalynb123 <gennalyn@live.com>
2021-11-19 12:04:32 -0800

    purpose page

--- app.py
+++ app.py
@@ -6,22 +6,24 @@ app = Flask(__name__)
 def index():
     return render_template("index.html") #"Avatar Website"
 
+#about pages
 @app.route("/Gennalyn/")
 def Gennalyn():
-    return render_template("Gennalyn.html") #"Avatar Website"
+    return render_template("Gennalyn.html")
 
 @app.route("/Kashish/")
 def Kashish():
-    return render_template("Kashish.html") #"Avatar Website"
+    return render_template("Kashish.html")
 
 @app.route("/Ellen/")
 def Ellen():
-    return render_template("Ellen.html") #"Avatar Website"
+    return render_template("Ellen.html")
 
 @app.route("/Sanvi/")
 def Sanvi():
-    return render_template("Sanvi.html") #"Avatar Website"
+    return render_template("Sanvi.html")
 
+#PBL pages
 @app.route("/FindYourNation/")
 def FindYourNation():
     return render_template("FindYourNation.html")
@@ -29,6 +31,10 @@ def FindYourNation():
 @app.route("/Training/")
 def Training():
     return render_template("Training.html")
+
+@app.route("/Purpose/")
+def Purpose():
+    return render_template("Purpose.html")
 
 
 

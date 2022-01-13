f4b09ff978f6854fd275d27454ef636763390fae
Ellen Xu <xu.ellen@outlook.com>
2022-01-13 11:39:47 -0800

    fix duplicate path to search

--- app.py
+++ app.py
@@ -62,10 +62,10 @@ def fire():
 def air():
     return render_template("air.html")
 
-@app.route('/search')
-def search():
+@app.route('/lookup')
+def lookup():
 
-    query = request.args.get('search')
+    query = request.args.get('lookup')
     # req_search = query.filter_by(req_no=query)
 
     page = query+'.html'
@@ -74,7 +74,7 @@ def search():
     if my_file.exists():
         return render_template(page)
     else:
-        return render_template('search.html', req_search=query)
+        return render_template('lookup.html', req_search=query)
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080)
